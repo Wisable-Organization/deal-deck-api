62eb3db97fba66501e19670680cb6acaed793e3d
jo-sanchez-gbh <jo.sanchez@gbh.tech>
2025-11-06 15:59:23 -0400

    Refactored into routes folder

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from .routes import router
-from .auth_routes import router as auth_router
+from .routes.auth import router as auth_router
 
 
 def create_app() -> FastAPI:
